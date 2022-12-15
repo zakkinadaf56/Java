7fdfb8892ddd7454fb3ab0e2b3c824135e02878a
Zakki <zakkinadaf56@gmail.com>
2022-12-15 22:43:47 +0530

    addition

---
+++ addition.java
@@ -0,0 +1,11 @@
+class addition
+{
+     public static void main(String[] args)
+    {
+        System.out.print("suym of the num,ber is :");
+         int num1=6, num2=9, num3=7, sum=num1+num2+num3;
+         System.out.println(sum);
+     
+        
+    }
+}
