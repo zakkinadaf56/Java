b006242092104a873749d83734fffe71c46334f4
zakkinadaf56 <zakkinadaf56@gmail.com>
2022-12-20 11:14:45 +0530

    patterns

---
+++ pattern.java
@@ -0,0 +1,17 @@
+class pattern 
+{
+    public static void main(String ar[]) 
+    {
+        int i,j;
+        for(i=1;i<=4;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(64);
+            }
+         System.out.println("");
+        }
+        
+    }
+    
+}
